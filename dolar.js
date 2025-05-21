d7084ad8af149cb1e838794e8b7c1ce4a32a7516
tsukita <guizhong960@gmail.com>
2025-05-21 08:44:14 -0300

    dolar

---
+++ dolar.js
@@ -0,0 +1,16 @@
+function dolar() {
+    const valor = document.getElementById("dolar").value;
+    const resultado = document.getElementById("resultado");
+    const cotacao = 5.69;
+ 
+    const valorDolar = Number(valor);
+ 
+    if (isNaN(valorDolar) || valor.trim() === '') {
+        resultado.innerHTML = "Por favor, insira um valor válido em dólares.";
+    } else {
+        let cambio = valorDolar * cotacao;
+        resultado.innerHTML = `R$ ${cambio.toFixed(2)}`;
+    }
+}
+ 
+
